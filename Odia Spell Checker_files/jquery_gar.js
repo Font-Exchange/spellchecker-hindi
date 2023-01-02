65921c822ec875317b4985e72cc26230aae9cbd8
Font-Exchange <94764286+Font-Exchange@users.noreply.github.com>
2023-01-02 11:45:43 +0530

    FEXCHAGE

---
+++ Odia Spell Checker_files/jquery_gar.js
@@ -0,0 +1,51 @@
+/*!***************************************************
+* Dictionary Auto Correction,
+* Script Ver 1.4
+* http://www.expoodisha.com
+* Copyright (c) 2018, Manas Ranjan Swain
+
+*****************************************************/
+
+$(document).ready(function() {
+    $('#fnrbtn').click(function() {
+    var textarea=$('#uInput');
+textarea.val(textarea.val().replace(/​‌େ​‌କ/g,"କେ"));
+textarea.val(textarea.val().replace(/‌‌େ​‌ଖ/g,"ଖେ"));
+textarea.val(textarea.val().replace(/‌‌େ​‌ଗ/g,"ଗେ"));
+textarea.val(textarea.val().replace(/‌‌େ​‌ଘ/g,"ଘେ"));
+textarea.val(textarea.val().replace(/‌​‌େ​‌ଚ/g,"ଚେ"));
+textarea.val(textarea.val().replace(/‌​‌େ​‌ଛ/g,"ଛେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଜ‌/g,"ଜେ"));
+textarea.val(textarea.val().replace(/‌‌େ​‌ଝ/g,"ଝେ"));
+textarea.val(textarea.val().replace(/‌​‌େ​‌ଟ/g,"ଟେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଠ‌/g,"ଠେ"));
+textarea.val(textarea.val().replace(/‌​‌େ​‌ଡ/g,"ଡେ"));
+textarea.val(textarea.val().replace(/‌​‌େ​‌ଡ଼/g,"ଡ଼େ"));
+textarea.val(textarea.val().replace(/‌​‌େ​‌ଢ/g,"ଢେ"));
+textarea.val(textarea.val().replace(/‌​‌େ​‌ଢ଼/g,"ଢ଼େ"));
+textarea.val(textarea.val().replace(/‌​‌େ​‌ଣ/g,"ଣେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ତ‌/g,"ତେ"));
+textarea.val(textarea.val().replace(/‌େ​‌ଥ/g,"ଥେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଦ/g,"ଦେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଧ/g,"ଧେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ନ/g,"ନେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ପ/g,"ପେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଫ/g,"ଫେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ବ/g,"ବେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଭ/g,"ଭେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ମ/g,"ମେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଯ/g,"ଯେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ର/g,"ରେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଳ/g,"ଳେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ବ/g,"ବେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଶ/g,"ଶେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଷ/g,"ଷେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ସ/g,"ସେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ହ/g,"ହେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌କ୍ଷ/g,"କ୍ଷେ"));
+textarea.val(textarea.val().replace(/​‌େ​‌ଲ/g,"ଲେ"));
+
+ 
+
+    });
+});
---
+++ Odia Spell Checker_files/wordConvertion.js
@@ -0,0 +1,16 @@
+﻿
+var wordConvrtionUnicode = [
+	["ं","ନ୍"],
+	["ୁ","ୂ"],
+	["ି","ୀ"],
+	["ଏ","ଐ"]
+]
+
+var wordConvrtionAkruti = [
+	["¢","‹"],
+	["é","ê"],
+	["ç","è"],
+	["ð","ñ"]
+]
+
+var wordConvrtion = wordConvrtionUnicode; 
